b1e940f1592cde693c9f8c9f95941a4549452f57
Yariangel Aray <gabrielamunar4@gmail.com>
2025-04-23 16:33:41 -0500

    Validaciones

--- 03 - eventos/app.js
+++ 03 - eventos/app.js
@@ -9,26 +9,75 @@ const documento = document.querySelector('[name="documento"]');
 const usuario = document.querySelector('[name="usuario"]');
 const contrasena = document.querySelector('[name="contrasena"]');
 
+const teclasEspeciales = ["Backspace", "Tab", "Enter", "ArrowLeft", "ArrowRight", "Delete"];
+
 //funciones
-const validar = (event) => {
+
+const validarTexto = (event) => {
+  const key = event.key;
+  const regex = /^[a-z\s]*$/i;
+  const letra = key;
+  if (!regex.test(letra)) {
+    event.preventDefault();
+  }
+}
+
+const validarNumero = (event) => {
+  const key = event.key;
+  const regex = /^[0-9]*$/;
+  const letra = key;
+  if (!regex.test(letra) && !teclasEspeciales.includes(letra)) {
+    event.preventDefault();
+  }
+}
+
+const validarCampos = (event) => {
   event.preventDefault();
+  let errores = [];
+  let regexContra = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+
   if (nombre.value.trim() == "") {
-    alert("Ingrese el nombre");
+    errores.push("Nombre");
     nombre.focus();
   }
   if (apellido.value.trim() == "") {
-    alert("Ingrese el apellido");
+    errores.push("Apellido");
     apellido.focus();
   }
   if (telefono.value.trim() == "") {
-    alert("Ingrese el teléfono");
+    errores.push("Telefono");
     telefono.focus();
   }
   if (documento.value.trim() == "") {
-    alert("Ingrese el documento");
+    errores.push("Documento");
     documento.focus();
+  }
+  if (usuario.value.trim() == "") {
+    errores.push("Usuario");
+    usuario.focus();
+  }
+  if (contrasena.value.trim() == "") {
+    errores.push("Contraseña");
+    contrasena.focus();
+  }
+  
+  if (errores.length > 1) {
+    alert(`Los siguientes campos son obligatorios: \n⊗ ${errores.join('\n ⊗ ')}` );
+  } 
+  else if (errores.length == 1) {
+    alert(`El siguiente campo es obligatorio: \n⊗ ${errores[0]}`);    
+  }
+
+  if (regexContra.test(contrasena.value) == false) {
+    alert("La contraseña debe tener al menos 8 caracteres, una mayúscula y un número");
+    contrasena.focus();
   }
 }
 
 //eventos
-formulario.addEventListener('submit', validar);
+nombre.addEventListener('keydown', validarTexto);
+apellido.addEventListener('keydown', validarTexto);
+telefono.addEventListener('keydown', validarNumero);
+documento.addEventListener('keydown', validarNumero);
+
+formulario.addEventListener('submit', validarCampos);
