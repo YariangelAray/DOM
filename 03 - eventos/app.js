b90745e912e7aa2ee726750a57fa06f1e28e2eb0
Yariangel Aray <gabrielamunar4@gmail.com>
2025-04-25 09:12:43 -0500

    Validaciones con check

--- 03 - eventos/app.js
+++ 03 - eventos/app.js
@@ -2,12 +2,15 @@
 
 // variables
 const formulario = document.querySelector('form');
+const boton = document.querySelector('#btn_validar');
+
 const nombre = document.querySelector('[name="Nombre"]');
 const apellido = document.querySelector('[name="Apellido"]');
 const telefono = document.querySelector('[name="Telefono"]');
 const documento = document.querySelector('[name="Documento"]');
 const usuario = document.querySelector('[name="Usuario"]');
 const contrasena = document.querySelector('[name="Contrasena"]');
+const checkBox = document.querySelector('[name="politica"]');
 
 const campos = [nombre, apellido, telefono, documento, usuario, contrasena];
 
@@ -31,45 +34,78 @@ const validarNumero = (event) => {
   }
 }
 
+const agregarError = (campo) => {
+  campo.classList.add('borde-rojo');
+  if (campo.nextElementSibling) campo.nextElementSibling.remove();
+  
+  let texto = document.createElement('span');
+  texto.textContent = `El campo es obligatorio.`;
+  texto.classList.add('texto-advertencia');
+  campo.insertAdjacentElement('afterend', texto); 
+}
+
+const quitarError = (campo) => {
+  campo.classList.remove('borde-rojo')
+  campo.nextElementSibling.remove();
+}
+
 const validarCampos = (event) => {
 
   event.preventDefault();
 
-  let camposVacios = [];
+  let mensaje = "";
   let regexContra = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).{8,}$/;
 
+  for (let i = 0; i < campos.length; i++) {    
 
-  campos.forEach(campo => {
-    if (campo.value.trim() == "") {
-      camposVacios.push(campo.name);  
+    if (campos[i].value.trim() == "") {               
+      agregarError(campos[i]);
     }
-  });
-
-  let mensaje = "";
-
-  if (camposVacios.length > 1) {
-    mensaje = `Los siguientes campos son obligatorios: \n⊗ ${camposVacios.join('\n ⊗ ')}\n`;    
-  } 
-  else if (camposVacios.length == 1) {
-    mensaje = `El siguiente campo es obligatorio: \n⊗ ${camposVacios[0]}\n`;    
-  }  
-
-  if (!regexContra.test(contrasena.value)) {
-    mensaje += "\nLa contraseña debe tener al menos 8 caracteres, una mayúscula y un número";    
+    else if (campos[i].className == 'borde-rojo') {
+      quitarError(campos[i]);              
+    }
   }
 
-  if (mensaje != "") {
-    alert(mensaje);    
-  } 
-  else {
-    alert("Formulario enviado correctamente");    
+  if (contrasena.value.trim() != "" && !regexContra.test(contrasena.value)) {
+    alert("La contraseña debe tener al menos 8 caracteres, una mayúscula, un número y un caracter especial");
+  }
+}
+
+const validarCampo = (event) => {
+  let campo = event.target;
+
+  if (campo.value.trim() == "") {
+    agregarError(campo);
+  }
+  else if(campo.className == 'borde-rojo') {
+    quitarError(campo)
+  }
+}
+
+const habilitarBoton = () => {  
+  if (!checkBox.checked) {
+    boton.setAttribute('disabled', '');
+  }
+  else if (boton.disabled) {
+    boton.removeAttribute('disabled');
   }
 }
 
 //eventos
+
+addEventListener('DOMContentLoaded', habilitarBoton);
+checkBox.addEventListener('change', habilitarBoton);
+
 nombre.addEventListener('keydown', validarTexto);
 apellido.addEventListener('keydown', validarTexto);
 telefono.addEventListener('keydown', validarNumero);
 documento.addEventListener('keydown', validarNumero);
+
+nombre.addEventListener('blur', validarCampo);
+apellido.addEventListener('blur', validarCampo);
+telefono.addEventListener('blur', validarCampo);
+documento.addEventListener('blur', validarCampo);
+usuario.addEventListener('blur', validarCampo);
+contrasena.addEventListener('blur', validarCampo);
 
 formulario.addEventListener('submit', validarCampos);
